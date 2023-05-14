c2e2e11ba07efb4bab38aa86c8f8d20f54ab0f43
Aishah Ahmeed <aishahahmeed@gmail.com>
2023-05-13 21:04:51 -0600

    Add password rules

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
 const container = document.getElementById("container");
-const passwordDiv = document.getElementById("userpassword");
+const passwordInput = document.getElementById("password_input");
+const pwBox = document.getElementById("password_div");
 const submitBtn = document.getElementById("submitbtn");
+const pw = document.getElementById("password_input");
+const cpw = document.getElementById("pwconfirm");
+const confPwBox = document.getElementById("confpassword");
 
 let formValid = false;
 let pwvalid = false;
 
 let confirm = () => {
-    let pw = document.getElementById("password");
-    let cpw = document.getElementById("pwconfirm");
-
     if (pwvalid = true) {
         cpw.style.backgroundColor = '#FFFFFF'
     }
@@ -16,7 +17,6 @@ let confirm = () => {
     if (pw.value !== '' && cpw.value !== '') {
         if (pw.value == cpw.value) {
             pwvalid = true;
-            console.log('match');
             pw.style.border = '1px solid #60993E'
             cpw.style.border = '1px solid #60993E'
         } else {
@@ -27,3 +27,33 @@ let confirm = () => {
         }
     }
 }
+
+function checkSubmission() {
+     if (pwvalid = false) {
+        return false;
+    }
+}
+
+// show password rules
+const pwRules = document.createElement('div');
+pwRules.setAttribute("class", "pw_rules");
+pwRules.innerText = 'Password must be at least 6 characters \n \
+Password should include at least one uppercase letter \n \
+Password should contain at least one number';
+
+
+pwBox.addEventListener("click", () => {
+    if (pw.value == '');
+    pwBox.appendChild(pwRules);
+})
+
+passwordInput.addEventListener("blur", () => {
+    pwRules.remove();
+})
+
+/*
+- click, show rules
+- if clicked again:
+if empty, keep showing rules
+if filled out and matches rules, hide rules
+*/
