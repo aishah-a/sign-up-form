83886d3e566a737fe0bfe8b89d22fc84592a03ad
Aishah <aishahahmeed@gmail.com>
2025-08-22 15:02:19 +0200

    Create password checker and show password rules

--- script.js
+++ script.js
@@ -1,13 +1,65 @@
 
-// check passwords
+
+
+
+// function checkpassword() {
+//     if (passwordInput.value === pwConfirmInput) {
+//         console.log("you typed" + pwConfirmInput);
+//         correctpassword = true;
+//     }
+// }
+
+// // show password rules on input focus
+
+// // create div containing password rules
+// const pwRules = document.createElement("div");
+// pwRules.style.backgroundColor = "pink";
+// pwRules.textContent = "hi there, bla bla bla";
+
+
+
+// password.addEventListener("focus", () => {
+
+//     console.log("in focus");
+//     password.appendChild(pwRules);
+//     }
+// )
+
+
+// password validation
+
+const pwDiv = document.querySelector(".password_div");
+const confirmDiv = document.querySelector(".confirm_div");
 const password = document.querySelector("#password");
-const pwConfirm = document.querySelector("#pw_confirm");
+const confirm = document.querySelector("#pw_confirm");
+
 
 let correctPassword = false;
 
+// check passwords match
+
 function checkPassword() {
-    if (password.value === pwConfirm) {
-        console.log("you typed" + password);
-        correctPassword = true
+    if (password.value === confirm.value) {
+        
+        correctPassword = true // add styling later
+
+        console.log("correct"); // remove
     }
 }
+
+const rules = document.createElement("div");
+let rulesText = "Password must be at least 8 characters. \n \ Password must contain at least 1 number. \n \ Password must contain at least 1 special character.";
+
+rules.innerText = rulesText;
+rules.setAttribute("class", "rules")
+
+
+password.addEventListener("focus", () => {
+        
+    pwDiv.appendChild(rules);
+})
+
+password.addEventListener("blur", () => {
+
+    rules.remove();
+})
