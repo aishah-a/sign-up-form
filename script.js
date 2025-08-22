bc9c314d8bf4a36806dcb1b0791e2974bb4d974e
Aishah <aishahahmeed@gmail.com>
2025-08-22 15:11:23 +0200

    Validate password confirm on form submit

--- script.js
+++ script.js
@@ -1,24 +1,13 @@
 // password validation
-
+const form = document.querySelector("form");
 const pwDiv = document.querySelector(".password_div");
 const confirmDiv = document.querySelector(".confirm_div");
 const password = document.querySelector("#password");
 const confirm = document.querySelector("#pw_confirm");
+const submit = document.querySelector("button");
 
 
-let correctPassword = false;
-
-// check passwords match
-
-function checkPassword() {
-    if (password.value === confirm.value) {
-        
-        correctPassword = true // add styling later
-
-        console.log("correct"); // remove
-    }
-}
-
+// show rules
 const rules = document.createElement("div");
 let rulesText = "Password must be at least 8 characters. \n \ Password must contain at least 1 number. \n \ Password must contain at least 1 special character.";
 
@@ -34,3 +23,23 @@ password.addEventListener("blur", () => {
 
     rules.remove();
 })
+
+// check passwords match
+
+let correctPassword = false;
+
+function checkPassword() {
+    if (password.value === confirm.value) {
+        correctPassword = true
+    }
+}
+
+// prevent form submission if incorrect password
+form.addEventListener("submit", function(e) {
+    checkPassword();
+    if (correctPassword === false) {
+        e.preventDefault();
+    }
+})
+
+
